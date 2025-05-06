e9115665261a3b75f6fb4f2537e890d9a3179c74
Leo <tsossachugar@gmial.com>
2025-05-05 23:01:23 -0400

    primer

---
+++ Cafeteria_back/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿namespace Cafeteria_back.Models
+{
+    public class Cliente
+    {
+       private int? id;
+       private string? nombre;
+       private string? apell_pater;
+       private string? nombre_mater;
+        private int? telefono;
+        private float? ubi_latitud;
+        private float? ubi_longitud;
+        private string? nom_ubicacion;
+        private int? NIT;
+        private string? user;
+        private string? password;
+
+
+
+    }
+}

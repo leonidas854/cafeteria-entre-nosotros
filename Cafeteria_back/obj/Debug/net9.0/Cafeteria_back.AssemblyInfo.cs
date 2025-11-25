9db884f32fb56b7febe036e78227624b7fdf2b3b
leo <tsosachugar@gmail.com>
2025-11-25 11:23:05 -0400

    api

--- Cafeteria_back/obj/Debug/net9.0/Cafeteria_back.AssemblyInfo.cs
+++ Cafeteria_back/obj/Debug/net9.0/Cafeteria_back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafeteria_back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be40703ba214ed8b4a8da8321234e92a84db94c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee1fe120e6de271a67ae80be495977a0e3dd0fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafeteria_back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cafeteria_back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
